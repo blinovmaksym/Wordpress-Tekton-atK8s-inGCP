ab39ae285e3ba5ec3ff5239767afebadf40b5bf0
blinovmaxim <blinov.maxim@gmail.com>
2023-06-02 16:55:55 +0300

    destroy

--- terraform_gcp/scripts/install-tekton.sh
+++ terraform_gcp/scripts/install-tekton.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-
 # Install Tekton CLI
 echo "Tekton CLI"
 curl -LO https://github.com/tektoncd/cli/releases/download/v0.30.1/tektoncd-cli-0.30.1_Linux-64bit.deb
 sudo dpkg -i ./tektoncd-cli-0.30.1_Linux-64bit.deb
 
 
-echo "Install Tekton Pipelines"
 # Install Tekton Pipelines
+echo "Install Tekton Pipelines"
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 
 # Install tasks from Tekton Hub
@@ -23,36 +22,6 @@ kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers
 echo "Install Tekton Operators"
 kubectl apply -f https://storage.googleapis.com/tekton-releases/operator/latest/release.yaml
 
-# if ! curl -s -H "Authorization: token $TOKEN_TEKTON" https://api.github.com/user/keys | grep -q "Tekton SSH Key"; then
-#     ssh-keygen -t rsa -b 4096 -C "tekton@tekton.dev" -f tekton_key
-#     # save as tekton / tekton_key.pub
-#     # Get the public key contents
-#     public_key=$(cat tekton_key.pub)
-#     # Create a new SSH key on GitHub
-#     curl -X POST -H "Authorization: token $TOKEN_TEKTON" -d '{"title":"Tekton SSH Key","key":"'"${public_key}"'"}' https://api.github.com/user/keys
-
-# # create secret YAML from contents
-# cat tekton_key | base64 -w 0 > tekton_key_base64.txt
-# cat > tekton-git-ssh-secret.yaml << EOM
-# apiVersion: v1
-# kind: Secret
-# metadata:
-#     name: git-ssh-key
-#     annotations:
-#         tekton.dev/git-0: github.com
-# type: kubernetes.io/ssh-auth
-# data:
-#     ssh-privatekey: $(cat tekton_key_base64.txt)
-# ---
-# EOM
-
-# kubectl apply -f tekton-git-ssh-secret.yaml
-# else    
-#     # Tekton SSH key exists on GitHub, skip generating and adding it
-#     echo "Tekton SSH key already exists on GitHub"
-
-# fi   
-# create your Docker registry secret, for example:
 cat > regsecret.yaml << EOM
 kind: Secret
 apiVersion: v1
@@ -69,17 +38,4 @@ EOM
 kubectl apply -f regsecret.yaml
 kubectl apply -f tekton/
 
-# # Create a triggers secret for GitHub:
-
-# cat > github-trigger-secret.yaml << EOM
-# apiVersion: v1
-# kind: Secret
-# metadata:
-#   name: github-trigger-secret
-# type: Opaque
-# stringData:
-#   secretToken: "123"
-# ---
-# EOM
-
-# kubectl apply -f github-trigger-secret.yaml
+kubectl create rolebinding pipeline-pvc --clusterrole=edit --serviceaccount=default:pipeline --namespace=default
